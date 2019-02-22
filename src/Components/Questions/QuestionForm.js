e813cec13a148b5d65aae16c0bb108ff4e10a981
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-02-22 13:01:04 +0100

    addded quiz title input

---
+++ src/Components/Questions/QuestionForm.js
@@ -0,0 +1,197 @@
+import React from "react";
+import { Formik, Form, FieldArray, ErrorMessage } from "formik";
+import styled from "styled-components";
+import { SingleLineTextInput, MultiLineTextInput } from "../Common/TextInputs";
+import { RadioButton, IconButton, TextButton } from "../Common/Buttons";
+import { IoMdClose, IoMdCreate } from "react-icons/io";
+
+const questionInitialValues = {
+  title: "",
+  answers: ["", ""],
+  correctAnswer: ""
+};
+
+const OverlayButtonsWrapper = styled.div`
+  opacity: 0;
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  transition: opacity 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+
+  :hover ${OverlayButtonsWrapper} {
+    opacity: 1;
+  }
+`;
+
+const QuestionGrid = styled.div`
+  display: grid;
+  grid-gap: 8px 8px;
+  margin: 8px 0px;
+  align-items: center;
+  grid-template-columns: min-content 1fr 1fr min-content;
+`;
+
+const GridQuestionIndex = styled.div`
+  font-size: 2rem;
+`;
+
+const GridMultiLineTextInput = styled(MultiLineTextInput)`
+  grid-column: 2 / span 3;
+`;
+const GridSingleLineTextInput = styled(SingleLineTextInput)`
+  grid-column: 2 / span 2;
+`;
+
+const AddAnswerButtonGrid = styled(TextButton)`
+  grid-column: 1 / span 4;
+`;
+
+const GridSubmitButton = styled(TextButton)`
+  grid-column: 1 / span 2;
+`;
+
+const GridCancelButton = styled(TextButton)`
+  grid-column: 3 / span 2;
+`;
+
+const validate = values => {
+  let errors = {};
+
+  if (!values.title) {
+    errors.title = "Required";
+  }
+  if (!values.answers || values.answers.some(answer => answer.length === 0)) {
+    errors.answers = "Invalid answers";
+  }
+  const correctAnswerIndex = parseInt(values.correctAnswer);
+  if (
+    isNaN(correctAnswerIndex) ||
+    correctAnswerIndex < 0 ||
+    correctAnswerIndex >= values.answers.length
+  ) {
+    errors.correctAnswer = "Invalid selected answer";
+  }
+
+  return errors;
+};
+
+const QuestionForm = props => (
+  <Wrapper>
+    <Formik
+      initialValues={props.question || questionInitialValues}
+      onSubmit={(values, actions) => {
+        props.onSubmit(values);
+      }}
+      enableReinitialize={true}
+      validate={validate}
+      onReset={() => console.log("resetting")}
+      render={({
+        values,
+        errors,
+        status,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        setFieldValue,
+        resetForm
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <QuestionGrid>
+            {props.index && (
+              <GridQuestionIndex>{`${props.index}.`}</GridQuestionIndex>
+            )}
+            <GridMultiLineTextInput
+              type="text"
+              name="title"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.title}
+              readOnly={props.readOnly}
+            />
+            <FieldArray
+              name="answers"
+              render={arrayHelpers => (
+                <React.Fragment>
+                  {values.answers &&
+                    values.answers.length > 0 &&
+                    values.answers.map((answer, index) => (
+                      <React.Fragment key={index}>
+                        <RadioButton
+                          type="radio"
+                          name="correctAnswer"
+                          value={index}
+                          onChange={handleChange}
+                          checked={parseInt(values.correctAnswer) === index}
+                          readOnly={props.readOnly}
+                        />
+                        <GridSingleLineTextInput
+                          type="text"
+                          name={`answers.${index}`}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={answer}
+                          readOnly={props.readOnly}
+                        />
+                        <IconButton
+                          type="button"
+                          onClick={() => {
+                            const correctIndex = parseInt(values.correctAnswer);
+                            if (correctIndex >= index) {
+                              setFieldValue("correctAnswer", correctIndex - 1);
+                            }
+                            arrayHelpers.remove(index);
+                          }}
+                          icon={<IoMdClose />}
+                          readOnly={props.readOnly}
+                          size="32px"
+                        />
+                      </React.Fragment>
+                    ))}
+                  {!props.readOnly && (
+                    <AddAnswerButtonGrid
+                      type="button"
+                      variant="sliced"
+                      onClick={() => arrayHelpers.push("")}
+                    >
+                      Add answer
+                    </AddAnswerButtonGrid>
+                  )}
+                </React.Fragment>
+              )}
+            />
+            {!props.readOnly && (
+              <React.Fragment>
+                <GridSubmitButton type="submit" variant="primary">
+                  Submit
+                </GridSubmitButton>
+                <GridCancelButton
+                  type="button"
+                  variant="secondary"
+                  onClick={() => {
+                    props.onCancel(values);
+                    resetForm();
+                  }}
+                >
+                  Cancel
+                </GridCancelButton>
+              </React.Fragment>
+            )}
+          </QuestionGrid>
+        </Form>
+      )}
+    />
+    {props.readOnly && (
+      <OverlayButtonsWrapper>
+        <IconButton onClick={props.onEdit} icon={<IoMdCreate />} size="36px" />
+        <IconButton onClick={props.onDelete} icon={<IoMdClose />} size="36px" />
+      </OverlayButtonsWrapper>
+    )}
+  </Wrapper>
+);
+export default QuestionForm;
--- src/Pages/CreateQuizPage.js
+++ src/Pages/CreateQuizPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import CreateQuestionForm from "../Components/Questions/CreateQuestionForm";
+import QuestionForm from "../Components/Questions/QuestionForm";
 import { TextButton } from "../Components/Common/Buttons";
+import { MultiLineTextInput } from "../Components/Common/TextInputs";
 
 const TemporaryWrapper = styled.div`
   width: 500px;
   margin: 16px auto;
+`;
+
+const TitleMultiLineInput = styled(MultiLineTextInput)`
+  :hover {
+    background: #e6e6e6;
+  }
 `;
 
 const CreateQuestionButton = styled(TextButton)`
@@ -20,6 +27,8 @@ const questionInitialValues = {
 
 export default class CreateQuizPage extends React.Component {
   state = {
+    titleFocused: true,
+    title: "",
     questions: [questionInitialValues],
     editedQuestion: 0,
     creatingQuestion: false
@@ -37,7 +46,6 @@ export default class CreateQuizPage extends React.Component {
 
   handleEditQuestionCancel = (index, values) => {
     if (this.state.questions[index] === questionInitialValues) {
-      console.log("edit canceled");
       this.setState(prevState => ({
         ...prevState,
         questions: prevState.questions.slice(0, -1),
@@ -76,17 +84,30 @@ export default class CreateQuizPage extends React.Component {
   render() {
     return (
       <TemporaryWrapper>
+        <TitleMultiLineInput
+          type="text"
+          name="title"
+          onChange={e => this.setState({ title: e.target.value })}
+          value={this.state.title}
+          readOnly={!this.state.titleFocused}
+          onFocus={() => this.setState({ titleFocused: true })}
+          onBlur={() => this.setState({ titleFocused: false })}
+          placeHolder="Quiz title"
+        />
+        <hr />
         {this.state.questions.map((question, index) => (
-          <CreateQuestionForm
-            key={index}
-            index={index + 1}
-            question={question}
-            onSubmit={values => this.handleQuestionSubmit(index, values)}
-            onCancel={values => this.handleEditQuestionCancel(index, values)}
-            readOnly={this.state.editedQuestion !== index}
-            onEdit={() => this.handleEditExistingQuestion(index)}
-            onDelete={() => this.handleDeleteExistingQuestion(index)}
-          />
+          <React.Fragment key={index}>
+            <QuestionForm
+              index={index + 1}
+              question={question}
+              onSubmit={values => this.handleQuestionSubmit(index, values)}
+              onCancel={values => this.handleEditQuestionCancel(index, values)}
+              readOnly={this.state.editedQuestion !== index}
+              onEdit={() => this.handleEditExistingQuestion(index)}
+              onDelete={() => this.handleDeleteExistingQuestion(index)}
+            />
+            <hr />
+          </React.Fragment>
         ))}
         {this.state.editedQuestion === "" && (
           <CreateQuestionButton
