5990260991887522cae245907577e453d46b7202
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-02-25 22:30:53 +0100

    creating front page sketch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import QuizEditorPage from "./Components/QuizEditorPage/QuizEditorPage";
 import { ThemeProvider } from "styled-components";
 import QuizSolvePage from "./Components/QuizSolvePage/QuizSolvePage";
+import FrontPage from "./Components/FrontPage/FrontPage";
 
 const theme = {
   color: {
@@ -15,7 +16,18 @@ const theme = {
     grayLight: "#f1f1f1",
     grayLightActive: "#e6e6e6",
     grayDark: "#a6a6a6",
-    accent: "#ffc107"
+    accent: "#ffc107",
+    primaryDarkOpacity: opac => `rgba(0, 121, 108, ${opac})`,
+    primaryLightOpacity: opac => `rgba(178, 223, 219, ${opac})`,
+    primaryOpacity: opac => `rgba(0, 150, 135, ${opac})`,
+    textOpacity: opac => `rgba(255, 255, 255, ${opac})`,
+    textPrimaryOpacity: opac => `rgba(33, 33, 33, ${opac})`,
+    textSecondaryOpacity: opac => `rgba(117, 117, 117, ${opac})`,
+    dividerOpacity: opac => `rgba(189, 189, 189, ${opac})`,
+    grayLightOpacity: opac => `rgba(241, 241, 241, ${opac})`,
+    grayLightActiveOpacity: opac => `rgba(230, 230, 230, ${opac})`,
+    grayDarkOpacity: opac => `rgba(166, 166, 166, ${opac})`,
+    accentOpacity: opac => `rgba(255, 193, 7, ${opac})`
   },
   animation: {
     duration: "0.3s",
@@ -28,7 +40,8 @@ class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         {/* <QuizEditorPage /> */}
-        <QuizSolvePage />
+        {/* <QuizSolvePage /> */}
+        <FrontPage />
       </ThemeProvider>
     );
   }
---
+++ src/Components/FrontPage/FrontPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import CreateQuizPage from "../QuizEditorPage/QuizForm";
+import { SingleLineTextInput } from "../Common/TextInputs";
+import { TextButton } from "../Common/Buttons";
+
+const PageWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TopContent = styled.div`
+  flex-grow: 2;
+  width: 100%;
+`;
+
+const BottomContent = styled.div`
+  flex-grow: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const SolveQuizPanel = styled.div`
+  width: 50%;
+  height: 100%;
+  text-align: center;
+  background-color: ${props => props.theme.color.primaryOpacity(0.5)};
+
+  :hover {
+    background-color: ${props => props.theme.color.primaryOpacity(0.65)};
+  }
+`;
+
+const CreateQuizPanel = styled.div`
+  width: 50%;
+  height: 100%;
+  text-align: center;
+  background-color: ${props => props.theme.color.grayLight};
+
+  :hover {
+    background-color: ${props => props.theme.color.grayLightActive};
+  }
+`;
+
+const QuizCodeInput = styled(SingleLineTextInput)`
+  display: inline-block;
+  margin: 16px 16px 4px 16px;
+  width: 50%;
+`;
+
+const SolveQuizButton = styled(TextButton)`
+  display: inline-block;
+  margin: 4px 16px 16px 16px;
+  width: 50%;
+`;
+
+const FrontPage = props => (
+  <PageWrapper>
+    <TopContent>Witaj tutaj</TopContent>
+    <BottomContent>
+      <SolveQuizPanel>
+        <QuizCodeInput />
+        <SolveQuizButton>Solve</SolveQuizButton>
+      </SolveQuizPanel>
+      <CreateQuizPanel>B</CreateQuizPanel>
+    </BottomContent>
+  </PageWrapper>
+);
+export default FrontPage;
